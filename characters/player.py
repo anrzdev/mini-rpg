adc7f61411f3625e1e325f8448d4fd0e94a4b3c9
anrz <anrz1502@gmail.com>
2024-04-28 18:56:21 -0300

    first

---
+++ characters/player.py
@@ -0,0 +1,6 @@
+
+class Player:
+  def __init__(self,name,level,life,):
+    self.name = name
+    self.level = level
+    self.life = life
---
+++ main.py
@@ -0,0 +1,9 @@
+# Importar la clase Player
+from characters.player import Player
+
+# Crear una instancia de Player
+player = Player("agus", 1, 100)
+
+print(player.name)
+print(player.level)
+print(player.life)
